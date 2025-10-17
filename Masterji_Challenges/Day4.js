f937757706f610ddec8ecb3a1666b89182c3577a
Nilesh Yadav <115637274+nyneil@users.noreply.github.com>
2025-10-17 14:15:50 +0000

    Datatypes and typeof

---
+++ Masterji_Challenges/Day4.js
@@ -0,0 +1,10 @@
+let userId=7
+let userName="Nilesh"
+let userAge= null 
+let userRole;
+
+console.table([userId, userName,userAge]);
+
+console.log(typeof userName);
+console.log(typeof userAge);
+console.log(typeof userRole);
