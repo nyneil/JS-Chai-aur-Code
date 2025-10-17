de1991090db1b38bf7458e5807f8194262799b84
Nilesh Yadav <115637274+nyneil@users.noreply.github.com>
2025-10-17 10:54:18 +0000

    Difference between Let, Const & Var with undefined.

---
+++ Masterji_Challenges/Day3.js
@@ -0,0 +1,14 @@
+const accountId = 070809;
+let accountEmail = "neil@test.com";
+var accountPassword ="01234";
+accountCity = "Mumbai";
+let accountState;
+
+// accountId= 123456
+accountEmail ="nilesh@test.com";
+accountPassword="56789";
+accountCity ="Navi Mumbai";
+
+//console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
