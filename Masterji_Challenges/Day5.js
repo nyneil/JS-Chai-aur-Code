62e92688d2706a18af2140273b78acda2e9428f1
Nilesh Yadav <115637274+nyneil@users.noreply.github.com>
2025-10-19 11:11:52 +0000

    Datatype Conversions

---
+++ Masterji_Challenges/Day5.js
@@ -0,0 +1,18 @@
+let score = 777;
+let name = "Nilesh";
+let age = "27nil"
+let age_number = Number(age);
+let isLoggedIn = 1;
+let booLoggedIn = Boolean(isLoggedIn);
+
+console.log(score);
+console.log(typeof score);
+console.log(name);
+console.log(typeof name);
+console.log(age)
+console.log(typeof age);
+console.log(typeof age_number);
+console.log(age_number);
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn);
+console.log(booLoggedIn);
